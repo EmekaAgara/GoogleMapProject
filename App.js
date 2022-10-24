1745f7939321b882c67a3ea494e0c9fbcd594598
Emeka Agara <emekaagara@gmail.com>
2022-10-24 21:13:41 +0100

    stack navigation

--- App.js
+++ App.js
@@ -2,16 +2,40 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import HomeScreen from './screens/HomeScreen';
+import BookTrip from './screens/BookTrip';
 import { store } from './store';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack'
 
 export default function App() {
+  const Stack = createStackNavigator();
   return (
 
     <Provider store={store}>
-      <SafeAreaProvider>
-        <HomeScreen />
-      </SafeAreaProvider>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <Stack.Navigator>
+            <Stack.Screen
+              name='HomeScreen'
+              component={HomeScreen}
+              options={{
+                headerShown:false,
+              }}
+            />
+
+            <Stack.Screen
+              name='BookTrip'
+              component={BookTrip}
+              options={{
+                headerShown:false,
+              }}
+            />
+          </Stack.Navigator>
+        </SafeAreaProvider>
+      </NavigationContainer>
+      
     </Provider>
   );
 }
