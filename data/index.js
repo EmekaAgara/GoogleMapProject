f694d76cce534f82529bd38146ebc32fac562dbd
Emeka Agara <emekaagara@gmail.com>
2022-11-02 20:08:53 +0100

    Logo and splash screens

---
+++ data/index.js
@@ -0,0 +1,37 @@
+export default [
+    {
+      id: 1,
+      img: require('../assets/watch7.jpeg'),
+      title: 'Book a Boat trip',
+      description: 'Book an affordable boat trip to your preferred destination',
+      price: '$0',
+    },
+    {
+      id: 2,
+      img: require('../assets/airpod.jpeg'),
+      title: 'Rent a Boat',
+      description: 'Rent your favorite boat and pay per hour',
+      price: '$0',
+    },
+    {
+      id: 3,
+      img: require('../assets/airpodmax.jpeg'),
+      title: 'Track a Boat',
+      description: 'Track a boat or vessel in the ocean and determine its Location, Longitude and Latitude and the ETA to your Location',
+      price: '$0',
+    },
+    {
+      id: 4,
+      img: require('../assets/charger.png'),
+      title: 'List a Boat',
+      description: "List your boat on this app and get paid daily for boat trips and tasks completed by your boat or vessel",
+      price: '$0',
+    },
+    {
+      id: 5,
+      img: require('../assets/lock.jpeg'),
+      title: 'Join our Team',
+      description: 'Join us today and get paid for completing tasks as a seaman,vessel owner,product manager or Influencer ',
+      price: '$0',
+    },
+  ];
