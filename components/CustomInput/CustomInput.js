7b1d8fc576946c4ce56b2294f4185824e002aa9a
Emeka Agara <emekaagara@gmail.com>
2022-11-03 14:40:24 +0100

    custom inputs done

--- components/CustomInput/CustomInput.js
+++ components/CustomInput/CustomInput.js
@@ -1,14 +1,15 @@
 import { StyleSheet, Text, View, TextInput } from 'react-native'
 import React from 'react'
 
-const CustomInput = ({value, setValue, placeholder}) => {
+const CustomInput = ({value, setValue, placeholder, secureTextEntry}) => {
   return (
     <View style={styles.container}>
       <TextInput 
         value={value}
         onChangeText={setValue}
-        placeholder='placeholder' 
-        style={styles.input} 
+        placeholder={placeholder}
+        style={styles.input}
+        secureTextEntry={secureTextEntry} 
         />
     </View>
   )
--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -2,14 +2,21 @@ import { StyleSheet, Text, View, Image, useWindowDimensions } from 'react-native
 import React from 'react'
 import Logo from '../assets/login.png'
 import CustomInput from '../components/CustomInput'
+import { useState } from 'react'
+
 
 const SignInScreen = () => {
+  const [ username, setUsername ] = useState('');
+  const [ password, setPassword ] = useState('');
+
+
   const {height} = useWindowDimensions();
   return (
     <View style={styles.root}>
       <Image source={Logo} style={[styles.logo, {height: height * 0.3}]} resizeMode='contain'/>
-      <CustomInput/>
-      <CustomInput/>
+      
+      <CustomInput placeholder="Username" value={username} setValue={setUsername}/>
+      <CustomInput placeholder="Password" value={password} setValue={setPassword} secureTextEntry />
     </View>
   )
 }
