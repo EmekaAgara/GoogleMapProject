20a7c151fc9f96efaf188cb69dc7f378f56a3fd2
Emeka Agara <emekaagara@gmail.com>
2022-11-04 14:02:02 +0100

    sign in screen navigation done

--- App.js
+++ App.js
@@ -48,7 +48,38 @@ export default function App() {
               }}
             />
 
+            <Stack.Screen
+              name='SignInScreen'
+              component={SignInScreen}
+              options={{
+                headerShown:false,
+              }}
+            />
 
+
+            <Stack.Screen
+              name='ForgotPasswordScreen'
+              component={ForgotPasswordScreen}
+              options={{
+                headerShown:false,
+              }}
+            />
+
+            <Stack.Screen
+              name='ConfirmEmailScreen'
+              component={ConfirmEmailScreen}
+              options={{
+                headerShown:false,
+              }}
+            />
+
+              <Stack.Screen
+              name='SignUpScreen'
+              component={SignUpScreen}
+              options={{
+                headerShown:false,
+              }}
+            />
 
 
             <Stack.Screen
