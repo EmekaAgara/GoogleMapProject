c41677c23cd57b62167653f215897200fc07d49e
Emeka Agara <emekaagara@gmail.com>
2022-11-06 22:38:44 +0100

    sign up authentication

--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, ScrollView, SafeAreaView } from 'react-native'
+import { StyleSheet, Text, View, ScrollView, SafeAreaView, Alert } from 'react-native'
 import React from 'react'
 import CustomInput from '../components/CustomInput'
 import { useState } from 'react'
@@ -6,18 +6,25 @@ import CustomButton from '../components/CustomButton'
 import SocialSignInButtons from '../components/SocialSignInButtons'
 import { useNavigation } from '@react-navigation/native'
 import { useForm } from 'react-hook-form'
+import { Auth } from 'aws-amplify'
+
 
 const EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
 
 const SignUpScreen = () => {
   const {control, handleSubmit, watch}  = useForm();
   const pwd = watch ('password');
-
-
   const navigation = useNavigation();
 
-  const onRegisterPressed=()=>{
-    navigation.navigate('ConfirmEmailScreen')
+  const onRegisterPressed = async (data) => {
+    const {name, username, password, email} = data;
+    try{
+      await Auth.signUp({username,password,attributes:{email, name, preferred_username: username},}); 
+      navigation.navigate('ConfirmEmailScreen')
+    } catch (e) {
+      Alert.alert('Chaiii', e.message);
+    }
+    
   }
 
   const onForgotPasswordPressed=()=>{
@@ -47,6 +54,7 @@ const SignUpScreen = () => {
       <View style={styles.root}>
         <Text style={styles.title}>Create a NAVR account</Text>
       
+        <CustomInput name="name" control={control} placeholder="Fullname" rules={{required:'Enter your Fullname',}}/>
         <CustomInput name="username" control={control} placeholder="Username" rules={{required:'Enter your username',}}/>
         <CustomInput name="email" control={control} placeholder="Email address" rules={{required:'Enter your email address', pattern: {value: EMAIL_REGEX, message:'Enter a valid email address'},}}/>
         <CustomInput name="password" control={control} placeholder="Password" secureTextEntry rules={{required:'Enter your password',}} />
