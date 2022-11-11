813fbedd89f2f0009245beb2fd105ad47b579540
Emeka Agara <emekaagara@gmail.com>
2022-11-11 17:12:27 +0100

    select option screen error fixed

--- slices/navSlice.js
+++ slices/navSlice.js
@@ -27,6 +27,6 @@ navSlice.actions;
 
 export const selectOrigin = (state) => state.nav.origin;
 export const selectDestination = (state) => state.nav.destination;
-export const selectTravelTimeInformation = (state) => state.nav.TravelTimeInformation;
+export const selectTravelTimeInformation = (state) => state.nav.travelTimeInformation;
 
 export default navSlice.reducer;
